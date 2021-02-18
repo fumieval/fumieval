1ca9ad150d1b60e376e9539395de734325146592
Fumiaki Kinoshita <fumiexcel@gmail.com>
2021-02-18 15:49:10 +0900

    add a script

---
+++ gen.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -ue
+
+REPOS="$(cat haskell-packages.txt)"
+
+echo "| Name | Stars | Issues | Merge Requests | Status | Hackage |"
+echo "| ---- | ----- | ------ | -------------- | ------ | ------- |"
+for REPO in $REPOS; do
+    echo "|$REPO"\
+        "|![Stars](https://img.shields.io/github/stars/fumieval/$REPO?style=social)"\
+        "| [![Issues](https://img.shields.io/github/issues/fumieval/$REPO?label=)](https://github.com/fumieval/$REPO/issues)"\
+        "| [![PRs](https://img.shields.io/github/issues-pr/fumieval/$REPO?label=)](https://github.com/fumieval/$REPO/pulls)"\
+        "| ![CI](https://github.com/fumieval/$REPO/workflows/Haskell%20CI/badge.svg)"\
+        "| [![Hackage](https://img.shields.io/hackage/v/$REPO.svg?label=)](https://hackage.haskell.org/package/$REPO)"
+done
