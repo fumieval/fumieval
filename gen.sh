7d841afe11311be95e4f1baa3834d43714c7fbbb
Fumiaki Kinoshita <fumiexcel@gmail.com>
2021-02-18 15:50:52 +0900

    fixup

--- gen.sh
+++ gen.sh
@@ -7,8 +7,8 @@ echo "| ---- | ----- | ------ | -------------- | ------ | ------- |"
 for REPO in $REPOS; do
     echo "|$REPO"\
         "|![Stars](https://img.shields.io/github/stars/fumieval/$REPO?style=social)"\
-        "| [![Issues](https://img.shields.io/github/issues/fumieval/$REPO?label=)](https://github.com/fumieval/$REPO/issues)"\
-        "| [![PRs](https://img.shields.io/github/issues-pr/fumieval/$REPO?label=)](https://github.com/fumieval/$REPO/pulls)"\
+        "| [![Issues](https://img.shields.io/github/issues/fumieval/$REPO?label=%22%22)](https://github.com/fumieval/$REPO/issues)"\
+        "| [![PRs](https://img.shields.io/github/issues-pr/fumieval/$REPO?label=%22%22)](https://github.com/fumieval/$REPO/pulls)"\
         "| ![CI](https://github.com/fumieval/$REPO/workflows/Haskell%20CI/badge.svg)"\
-        "| [![Hackage](https://img.shields.io/hackage/v/$REPO.svg?label=)](https://hackage.haskell.org/package/$REPO)"
+        "| [![Hackage](https://img.shields.io/hackage/v/$REPO.svg?label=%22%22)](https://hackage.haskell.org/package/$REPO)"
 done
